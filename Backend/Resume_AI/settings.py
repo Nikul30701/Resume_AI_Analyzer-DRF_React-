c27badf27306a3a86f5b1617ba13897b57032d1e
Nikul <p.nikul6403@gmail.com>
2026-03-02 18:51:06 +0530

    Production API URL set

--- Backend/Resume_AI/settings.py
+++ Backend/Resume_AI/settings.py
@@ -174,7 +174,8 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
 ]
-CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = False
 
 
 # Default primary key field type

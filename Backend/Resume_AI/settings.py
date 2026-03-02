ea6cbe39711dcb59d770cbd421ed0ca2ce4c9761
Nikul <p.nikul6403@gmail.com>
2026-03-02 14:46:51 +0530

    add guicorn to deploy on render

--- Backend/Resume_AI/settings.py
+++ Backend/Resume_AI/settings.py
@@ -104,7 +104,8 @@ WSGI_APPLICATION = 'Resume_AI.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.getenv("DATABASE_URL")
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600
     )
 }
 
